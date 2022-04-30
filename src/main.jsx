e6777a87d51610144f73b857242a955f1e535556
Satyam Kulkarni <40713709+sk1122@users.noreply.github.com>
2022-04-30 14:00:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import ReactGA from "react-ga"
-
-ReactGA.initialize('UA-226967881-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
